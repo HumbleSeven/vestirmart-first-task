5d62c69bdab134f0cb365b8ce65bdec1d9295f3a
HumbleSeven <obeng.sabbath@yahoo.com>
2023-08-03 02:52:26 +0100

    Update 2

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-// import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
 
 @Component({
@@ -7,13 +6,7 @@ import { HousingLocation } from '../housinglocation';
   templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css'],
   standalone: true,
-  // template: `
-  // <section class="listing">
-  //   <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-  //   <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-  //   <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-  // </section>
-  // `,
+
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
