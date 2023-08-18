d3ef8280928cfbb5849d2fcbd36fe8987e9062ba
HumbleSeven <obeng.sabbath@yahoo.com>
2023-08-18 09:04:42 +0100

    CSS Colors and Fonts Defined

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { HousingLocation } from '../housinglocation';
 @Component({
   selector:"app-home",
   templateUrl:"./home.component.html",
-  styleUrls:["./home.component.css"],
+  styleUrls:["./home.component.css", "../../styles/base.scss"],
   standalone:true,
   imports:[
     CommonModule, HousingLocationComponent
